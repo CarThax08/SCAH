eb81e9af4a96e75a252b6609e0ab5a2b27846271
CarThax08 <carson.thackston@gmail.com>
2021-01-06 12:19:17 -0500

    Added some MORE of the fundamentals for a working AH System.

--- src/main/java/com/github/carthax08/scah/Main.java
+++ src/main/java/com/github/carthax08/scah/Main.java
@@ -44,9 +44,17 @@ public final class Main extends JavaPlugin {
         meta2.setDisplayName("Decrease Page");
         item2.setItemMeta(meta2);
 
-        for(Inventory i : GUI){
-            i.setItem(53, item);
-            i.setItem(45, item2);
+        ItemStack item3 = new ItemStack(Material.MAGMA_CREAM);
+        ItemMeta meta3 = item3.getItemMeta();
+        meta3.setDisplayName("1");
+        for(int i = 0; i < AH_GUI.length; i++){
+            if(i > 0){
+                meta3.setDisplayName(String.valueOf(i));
+            }
+            item3.setItemMeta(meta3);
+            AH_GUI[i].setItem(53, item);
+            AH_GUI[i].setItem(45, item2);
+            AH_GUI[i].setItem(49, item3);
         }
     }
 
@@ -54,8 +62,9 @@ public final class Main extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
-    public static void openAuctionHouse(Player p){
-        p.openInventory(AH_GUI[0]);
+    public static void openAuctionHouse(Player p, int page){
+        //For page, 0 is the first page
+        p.openInventory(AH_GUI[page]);
     }
     public static Inventory[] getAuctionHouse(){
         return AH_GUI;
--- src/main/java/com/github/carthax08/scah/events/GuiClickEventHandler.java
+++ src/main/java/com/github/carthax08/scah/events/GuiClickEventHandler.java
@@ -1,10 +1,10 @@
 package com.github.carthax08.scah.events;
 
+import com.github.carthax08.scah.Main;
 import com.github.carthax08.simplecurrencies.SimpleCurrencies;
 import com.github.carthax08.simplecurrencies.api.Config;
 import com.github.carthax08.simplecurrencies.api.Currencies;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,25 +13,31 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class GuiClickEventHandler implements Listener {
     @EventHandler
     public void onGuiClickEvent(InventoryClickEvent e){
         if(e.getView().getTitle().equals("Auction House")){
-            ItemStack item = e.getClickedInventory().getItem(e.getSlot());
-            if(item.getItemMeta().getDisplayName().equals("Increase Page") || item.getItemMeta().getDisplayName().equals("Decrease Page")){
-                //WIP
-                System.out.println("ree");
-            }
-            if(Config.checkCurrency(item.getItemMeta().getLore().get(2))){
-                String currency = item.getItemMeta().getLore().get(2);
-                if(e.getWhoClicked() instanceof Player) {
-                    Player player = (Player) e.getWhoClicked();
-                    if(Currencies.getCurrency(currency, (Player) e.getWhoClicked()) >= Double.parseDouble(item.getItemMeta().getLore().get(1))){
+            if(e.getWhoClicked() instanceof Player) {
+                Player player = (Player) e.getWhoClicked();
+                ItemStack item = e.getClickedInventory().getItem(e.getSlot());
+                if (item.getItemMeta().getDisplayName().equals("Increase Page")) {
+                    if (Main.getAuctionHouse().length > Integer.parseInt(e.getInventory().getItem(49).getItemMeta().getDisplayName())) {
+                        Main.openAuctionHouse(player, Integer.parseInt(e.getInventory().getItem(49).getItemMeta().getDisplayName()) + 1);
+                    }
+                }
+                if (item.getItemMeta().getDisplayName().equals("Decrease Page")) {
+                    if (Main.getAuctionHouse().length != Integer.parseInt(e.getInventory().getItem(49).getItemMeta().getDisplayName()) && !e.getInventory().getItem(49).getItemMeta().getDisplayName().equals("1")) {
+                        Main.openAuctionHouse(player, Integer.parseInt(e.getInventory().getItem(49).getItemMeta().getDisplayName()) - 1);
+                    }
+                    System.out.println("ree");
+                }
+                if (Config.checkCurrency(item.getItemMeta().getLore().get(2))) {
+                    String currency = item.getItemMeta().getLore().get(2);
+                    if (Currencies.getCurrency(currency, (Player) e.getWhoClicked()) >= Double.parseDouble(item.getItemMeta().getLore().get(1))) {
                         Currencies.removeCurrency(currency, player, Double.parseDouble(item.getItemMeta().getLore().get(1)));
-                        Currencies.addCurrency(currency, Bukkit.getOfflinePlayer(item.getItemMeta().getLore().get(0)),Double.parseDouble(item.getItemMeta().getLore().get(1)));
+                        Currencies.addCurrency(currency, Bukkit.getOfflinePlayer(item.getItemMeta().getLore().get(0)), Double.parseDouble(item.getItemMeta().getLore().get(1)));
                         ItemStack item2 = e.getCurrentItem();
                         ItemMeta meta = item2.getItemMeta();
                         List<String> lore = new ArrayList<String>();
