6f3f0b85053ebf688e9d309bd3ae487bcd641e1c
CarThax08 <carson.thackston@gmail.com>
2021-01-06 09:54:25 -0500

    Added some of the fundamentals for a working AH System.

--- src/main/java/com/github/carthax08/scah/Main.java
+++ src/main/java/com/github/carthax08/scah/Main.java
@@ -2,6 +2,7 @@ package com.github.carthax08.scah;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -10,7 +11,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
-    private static Inventory AH_GUI;
+    private static Inventory[] AH_GUI;
+    private static int amountOfItems;
 
     @Override
     public void onEnable() {
@@ -20,21 +22,32 @@ public final class Main extends JavaPlugin {
         saveDefaultConfig();
         getServer().getConsoleSender().sendMessage("[SCAH] Config loaded....");
         if(!getConfig().isSet("ahgui")) {
-            AH_GUI = Bukkit.createInventory(null, 54);
+            AH_GUI[0] = Bukkit.createInventory(null, 54);
+            amountOfItems = 0;
             getConfig().set("ahgui", AH_GUI);
+            getConfig().set("amountOfItems", amountOfItems);
             addBasicItems(AH_GUI);
         }else{
-            AH_GUI = (Inventory) getConfig().get("ahgui");
+            AH_GUI = (Inventory[]) getConfig().get("ahgui");
+            amountOfItems = getConfig().getInt("amountOfItems");
         }
     }
 
-    private static void addBasicItems(Inventory GUI) {
+    private static void addBasicItems(Inventory[] GUI) {
         ItemStack item = new ItemStack(Material.ARROW, 1);
         ItemMeta meta = item.getItemMeta();
         meta.setDisplayName("Increase Page");
         item.setItemMeta(meta);
 
-        GUI.setItem(45, item);
+        ItemStack item2 = new ItemStack(Material.ARROW, 1);
+        ItemMeta meta2 = item2.getItemMeta();
+        meta2.setDisplayName("Decrease Page");
+        item2.setItemMeta(meta2);
+
+        for(Inventory i : GUI){
+            i.setItem(53, item);
+            i.setItem(45, item2);
+        }
     }
 
     @Override
@@ -42,9 +55,9 @@ public final class Main extends JavaPlugin {
         // Plugin shutdown logic
     }
     public static void openAuctionHouse(Player p){
-        p.openInventory(AH_GUI);
+        p.openInventory(AH_GUI[0]);
     }
-    public static Inventory getAuctionHouse(){
+    public static Inventory[] getAuctionHouse(){
         return AH_GUI;
     }
 }
